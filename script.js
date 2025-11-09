6fd3d5c4b75b3bcce027681de426eddf3e0e6704
carro._.t <evanathomson@gmail.com>
2025-11-09 14:38:21 -0800

    ts probs wont work

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const buttons = document.querySelectorAll('nav button');
+/*const buttons = document.querySelectorAll('nav button');
 const sections = document.querySelectorAll('section');
 
 buttons.forEach(btn => {
@@ -13,4 +13,11 @@ buttons.forEach(btn => {
         }, 1500); // wait ~1.5s after hello finishes
     });
 
+});*/
+
+window.addEventListener("DOMContentLoaded", () => {
+    // Wait for "Hello" + "I'm Evana Thomson" to animate first (~1.5s)
+    setTimeout(() => {
+        document.body.classList.add("loaded");
+    }, 1800); // small delay (~300ms after hello animation)
 });
